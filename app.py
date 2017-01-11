67912362061a23debd268d016d19ff24ba01382d
Austin <austinbrown34@gmail.com>
2017-01-11 03:16:40 -0800

    beginning of slack create channel

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 
+def create_slack_channel(text, token):
+    url = 'https://slack.com/api/channels.create'
+    payload = {"token": token, "name": text}
+    r = requests.post(url, json=json.dumps(payload))
+    print r.status_code
+
+
 @app.route('/')
 def hello():
     errors = []
@@ -18,7 +25,7 @@ def hello():
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     errors = []
-    results = {'msg': 'yay!', 'errors': errors}
+    results = {'msg': '', 'errors': errors}
     if request.method == "POST":
         token = request.form.get('token')
         if token != app.config['INTEGRATION_TOKEN']:
@@ -27,11 +34,16 @@ def create():
                 'until token is corrected. Try setting the '
                 'SLACK_INTEGRATION_TOKEN environment variable'
             )
-            results['msg'] = message
 
+        else:
+            message = (
+                'Successfully Create New Project!'
+            )
+        results['msg'] = message
         response_url = request.form.get('response_url')
         text = request.form.get('text')
         print 'This is the response_url: {}. This is the text: {}'.format(response_url, text)
+        create_slack_channel(text, token)
     json_results = json.dumps(results)
     return json_results
 
