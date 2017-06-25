e394e0b65d4c4af25c91e9a44148f7a89560bc11
lucid-kyle <kyle@lucidsf.com>
2017-06-25 15:43:49 -0700

    get ProjectID from LucidData create
    refactored lucid_api to create lucid data first, then call luciddata.rename later to create slug and store slack channel
    
    added status code checks to luciddata and have create() return new project id

--- lucid_api/services/lucid_data_service.py
+++ lucid_api/services/lucid_data_service.py
@@ -19,12 +19,12 @@ class LucidDataService(service_template.ServiceTemplate):
 
         self._logger = self._setup_logger(to_file=True)
 
-    def create(self, project_id, title, slack_channel, silent=None):
+    def create(self, title, slack_channel="", silent=None):
         '''Creates the database entry on luciddata'''
-        self._logger.info("Attempting to create id#%s: %s (slack: %s) on Lucid Data",
-            project_id, title, slack_channel)
+        self._logger.info("Attempting to create %s (slack: %s)",
+            title, slack_channel)
 
-        slug = self._format_slug(project_id,title)
+        slug = self._format_slug(0,title)
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
         url = '{}?username={}&api_key={}'.format(os.environ['PROJECT_API_BASE_URL'], os.environ['API_USERNAME'], os.environ['API_KEY'])
         payload = {
@@ -34,9 +34,14 @@ class LucidDataService(service_template.ServiceTemplate):
 
         }
         r = requests.post(url, data=json.dumps(payload), headers=headers)
-        
-        self._logger.info("LucidData response: %s\n\tresponse content: %s", r, r.content)
-        return r
+
+        if r.status_code == 201:
+            # Location looks like '/api/project/128/'
+            project_id = r.headers['Location'].rsplit("/",2)[-2]
+            return project_id
+        else:
+            self._logger.error("Got a bad status code: %s Headers: %s", r.status_code, r.headers)
+            raise LucidDataServiceError("Got the wrong status code from create: {} [{}]".format(r.status_code, r.headers))
 
     def archive(self, project_id):
         self._logger.info("Attempting to archive id#%s on Lucid Data", project_id)
@@ -53,7 +58,7 @@ class LucidDataService(service_template.ServiceTemplate):
         return r
 
 
-    def rename(self, project_id, new_title):
+    def rename(self, project_id, new_title, slack_channel=""):
         self._logger.info("Attempting to rename id#%s to %s on Lucid Data", project_id, new_title)
 
         new_slug = self._format_slug(project_id,new_title)
@@ -63,11 +68,18 @@ class LucidDataService(service_template.ServiceTemplate):
             'title': new_title,
             'slug': new_slug
         }
-        r = requests.put(url, data=json.dumps(payload), headers=headers)
-        
-        self._logger.info("LucidData response: %s\n\tresponse content: %s", r, r.content)
-        return r
 
+        if slack_channel is not "":
+            payload['slack_channel'] = slack_channel
+
+        r = requests.put(url, data=json.dumps(payload), headers=headers)
+
+        if r.status_code in range(200,299):
+            self._logger.info("Rename Success!")
+            return True
+        else:
+            self._logger.error("Rename failture. Status: %s", r.status_code)
+            raise LucidDataServiceError("Failture. Status: {}".format(r.status_code))
 
     def get_project_id_from_channel(self, slack_channel):
         self._logger.info("Attempting to get project id from slack channel %s on Lucid Data", slack_channel)
@@ -99,3 +111,7 @@ class LucidDataService(service_template.ServiceTemplate):
 
     def get_link_dict(self, project_id):
         return {":floppy_disk: "+self.get_pretty_name() : self.get_link(project_id)}
+
+
+class LucidDataServiceError(Exception):
+    pass
