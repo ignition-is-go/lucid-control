d6752e0a7f4b48f2ca6f385ad56cd9434396a42f
ewokninja <jt@lucidsf.com>
2017-06-25 20:22:49 -0700

    fixed typo

--- lucid_api_tests/make_envs.py
+++ lucid_api_tests/make_envs.py
@@ -8,7 +8,7 @@ import os
 with open('.env','r') as fp:
     document = fp.read()
 
-    # regex search for key and values in the .env file and assigne to variables
+    # regex search for key and values in the .env file and assign to variables
     d = re.findall(r'(?P<key>[\w\d_]+)=\"(?P<value>[^\"]*)\"\n?', document)
 
     # write the key and value pairs to the os environment
