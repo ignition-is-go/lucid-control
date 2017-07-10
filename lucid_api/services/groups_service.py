a34282bdda28e61f7a2874e72d4223923142f3f1
ewokninja <jt@lucidsf.com>
2017-07-10 17:54:19 -0400

    Group Settings Problems

--- lucid_api/services/groups_service.py
+++ lucid_api/services/groups_service.py
@@ -34,6 +34,7 @@ class GroupsService(service_template.ServiceTemplate):
         self._logger.info('Start Create Google Group for Project ID %s: %s', project_id, title)
 
         group = self._admin.groups()
+        grp_settings = self._group.groups()
         slug = self._format_slug(project_id, title)
         reg = r'^([\w]+-[\w]+)'
 
@@ -41,11 +42,15 @@ class GroupsService(service_template.ServiceTemplate):
             "email" : "{}@lucidsf.com".format(re.match(reg, slug).group()), # email address of the group
             "name" : slug, # group name
             "description" : "Group Email for {}".format(slug), # group description
+        }
+
+        # Setup our default settings.
+        dir_info = {
             "showInGroupDirectory" : "true", # let's make sure this group is in the directory
         }
 
         try:
-            create_response = group.insert(body=grp_info).execute()
+            create_response = group.insert(body=grp_info).grp_settings.patch(groupUniqueId=grp_info['email'], body=dir_info).execute() # This is where I left off - groupUniqueId unexpected arg
             self._logger.info('Created Google Group %s (ID: %s) with email address %s', grp_info['name'], project_id, grp_info['email'])
             self._logger.debug('Create response = %s', create_response)
         except errors.HttpError as err:
@@ -77,62 +82,67 @@ class GroupsService(service_template.ServiceTemplate):
 
         # 1. Check to see if the group even exists
         try:
-            group_id = get_group_id(project_id)
-        except GroupServiceError as err:
+            group_id = self.get_group_id(project_id)
+        except GroupsServiceError as err:
             self._logger.debug('Group with project ID %s does not exist.', project_id)
             raise GroupsServiceError("Could not find a project with ID # %s", project_id)
-                
-        service = _create_admin_service()
-        group = service.groups()
+        
+        group = self._admin.groups()
         slug = self._format_slug(project_id, new_title)
 
         # 2. Create the JSON request for the changes we want
         grp_info = {
-            "email" : "{}@lucidsf.com".format(slug), # new email address for the group
+            # We leave out 'email' because we want the address to remain the same.
             "name" : slug, # new group name
             "description" : "Group Email for {}".format(slug), # new group description
         } 
 
-        # 3. Perform actual rename here. Dictionary API
-        create_response = service.groups().patch(groupUniqueId=group_id, body=grp_info).execute()
-        self._logger.debug("Renamed Group ID %s to %s", project_id, slug)
+        # 3. Perform actual rename here. 
+        try:
+            create_response = self._admin.groups().patch(groupKey=group_id, body=grp_info).execute()
+            self._logger.debug("Renamed Group ID %s to %s", project_id, slug)
+        except GroupsServiceError as err:
+            self._logger.error('Unable to rename group %s to %s', project_id, new_title)
+            raise GroupsServiceError('Unable to rename group %s to %s', project_id, new_title)
+
+        return ['id']
 
     
     def archive(self, project_id):
         '''
-        Deletes an existing google group.
+        Archives an existing google group.
+        Read: Change the ShowInDirectory to False.
         '''
 
-        # At last discussion we did not want to archive / delete any google groups so we'll opt out here
-        return True
-
-        self._logger.info("Start Delete Google Group for Project ID %s", project_id)
+        self._logger.info("Started Archive Google Group for Project ID %s", project_id)
 
         # 1. Check to see if the group even exists
         try:
-            group_id = get_group_id(project_id)
-        except GroupServiceError as err:
-            self._logger.debug("Group with project ID %s does not exist.", project_id)
-            raise GroupsServiceError("Could not find a project with ID # %s", project_id)
-        
-        service = _create_admin_service
-        group = directory.groups()
-        
-        # 2. Delete the group
-        try:
-            archive_response = group.delete(group_id).execute()
-            self._logger.info("Deleted group with ID # %s", project_id)
-            return archive_response.body['ok']
+            group_id = self.get_group_id(project_id)
         except GroupsServiceError as err:
-            self._logger.error("Unable to delete Google Group with ID # %s", project_id)
-            GroupsServiceError("Unable to delete Google Group with ID # %s because: %s", project_id, err.message)
+            self._logger.error("Group with project ID %s does not exist.", project_id)
+            raise GroupsServiceError("Can't archive, no project ID # %s", project_id)
+        
+        group = self._admin.groups()
+        grp_info = { "showInGroupDirectory" : "false", }
+        
+        # 2. Remove the group from the directory
+        try:
+            create_response = self._admin.groups().patch(groupKey=group_id, body=grp_info).execute()
+            self._logger.info("Removed group with ID # %s from directory.", project_id)
+            return create_response.body['ok']
+        except GroupsServiceError as err:
+            self._logger.error("Unable to remove Google Group with ID # %s from directory", project_id)
+            GroupsServiceError("Ack! Can't remove ID # %s from directory because: %s", project_id, err.message)
+        
+        return ['id']
 
     
     def get_group_id(self, project_id):
-        service = _create_admin_service()
-        group = service.groups()
+        group = self._admin.groups()
 
         response = group.list(customer='my_customer').execute()
+        project_id = str(project_id)
 
         for i in response['groups']:
             if project_id in i['name']:
@@ -171,7 +181,7 @@ class GroupsService(service_template.ServiceTemplate):
             return service
     
     def _create_groupsettings_service(self):
-        scopes = ['https://www.googleapis.com/auth/admin.directory.group.member']
+        scopes = ['https://www.googleapis.com/auth/apps.groups.settings']
 
         with open("auths/lucid-control-b5aa575292fb.json", 'r') as fp:
             credentials = ServiceAccountCredentials.from_json_keyfile_dict(
