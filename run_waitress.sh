fb36042c23e07079dce7d9b83aa482a08622ad04
Kyle <kyle@lucidsf.com>
2018-01-01 11:12:42 -0700

    run waitress without su

--- run_waitress.sh
+++ run_waitress.sh
@@ -5,8 +5,9 @@ sleep 10
 
 cd /app/django  
 # prepare init migration
-su -m myuser -c "python manage.py makemigrations"  
+python manage.py makemigrations
 # migrate db, so we have the latest db schema
-su -m myuser -c "python manage.py migrate"  
+python manage.py migrate
 
-su -m myuser -c "waitress-serve --port=$PORT lucidcontrol.wsgi:application"
+# run waitress
+waitress-serve --port=$PORT lucidcontrol.wsgi:application
