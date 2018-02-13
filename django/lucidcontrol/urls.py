6e7a9f9e0387303e7492fed18e2f6ef94bc822b8
Kyle <kyle@lucidsf.com>
2018-02-12 22:18:56 -0800

    bad import in urls

--- django/lucidcontrol/urls.py
+++ django/lucidcontrol/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.settings import settings
+from django.conf import settings 
 
 urlpatterns = [
     url(r'^api/', include('lucid_api.urls', namespace='api')),
