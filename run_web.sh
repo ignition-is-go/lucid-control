b1e22963ca4e965dcf28e4ffb04e44c9cadd249e
Kyle Bjordahl <kyle@lucid.rocks>
2019-01-21 16:26:45 -0800

    fix(run): implement PORT env in run_web

--- run_web.sh
+++ run_web.sh
@@ -9,5 +9,5 @@ su -m myuser -c "python manage.py makemigrations lucid_api checkin"
 # migrate db, so we have the latest db schema
 su -m myuser -c "python manage.py migrate"  
 # start development server on public ip interface, on port 8000
-su -m myuser -c "python manage.py runserver 0.0.0.0:8000"
+su -m myuser -c "python manage.py runserver 0.0.0.0:$PORT"
 # su -m myuser -c "gunicorn lucidcontrol.wsgi --log-level=-"
