3a9330e7eaa51cfffdd91e4cec234d7a4fc4d42e
Kyle Bjordahl <kyle@lucid.rocks>
2019-01-21 18:28:10 -0800

    chore():formatting

--- django/lucid_api/urls.py
+++ django/lucid_api/urls.py
@@ -5,17 +5,21 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.api_root, name="root"),
+
+    # rest api
     url(r'^projects/$', views.ProjectList.as_view(), name="project_list"),
-    url(r'^projects/(?P<pk>[0-9]+)$', views.ProjectDetail.as_view(), name="project_detail"),
+    url(r'^projects/(?P<pk>[0-9]+)$',
+        views.ProjectDetail.as_view(), name="project_detail"),
+
     # Slack command handlers
     url(
-        r'^slack/action-response/$', 
+        r'^slack/action-response/$',
         views.action_response,
         name="Slack Action Response"
     ),
     url(
-        r'^slack/(?P<command>[a-z]+)/$', 
-        views.slash_command, 
+        r'^slack/(?P<command>[a-z]+)/$',
+        views.slash_command,
         name="Slack Command"
     ),
 ]
