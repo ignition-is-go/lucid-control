12a45f7dd73b836d21ce9554ba8a9f0fd6044118
Kyle Bjordahl <kyle@lucid.rocks>
2019-01-22 12:52:46 -0800

    feat(celery): run celery without gossip, mingle and heartbeat for logging cleanliness

--- run_beat.sh
+++ run_beat.sh
@@ -7,4 +7,4 @@ sleep 10
 cd /app/django  
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 # su -m myuser -c "celery worker -A myproject.celeryconf -Q default -n default@%h"
-su -m myuser -c "celery -A lucidcontrol beat -l debug --scheduler=django_celery_beat.schedulers:DatabaseScheduler"
+su -m myuser -c "celery -A lucidcontrol beat -l debug --scheduler=django_celery_beat.schedulers:DatabaseScheduler -without-gossip --without-mingle --without-heartbeat"
