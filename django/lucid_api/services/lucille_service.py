04d9bfdfa7330a816001f0b82b857459f5c3a9a5
Kyle Bjordahl <kyle@lucid.rocks>
2019-02-12 18:44:08 -0800

    feat(lucille_service): add archive and unarchive mutations
    fix upsert mutation format

--- django/lucid_api/services/lucille_service.py
+++ django/lucid_api/services/lucille_service.py
@@ -26,18 +26,11 @@ class Service(service_template.ServiceTemplate):
     _upsert_mutation = '''
 	mutation {
 		upsertProject(
-			where: {
-				lucidId: {p.id}
+			lucidId: {p.id}
+			name: {p.title}
+			typeCode: {p.type_code}
 			}
-			create: {
-				lucidId: {p.id}
-				name: {p.title}
-				typeCode: {p.type_code}
-			}
-			update: {
-				name: {p.title}
-				typeCode: {p.type_code}
-			}
+			
 		){
 			id
 			slug
@@ -45,6 +38,27 @@ class Service(service_template.ServiceTemplate):
 	}
 	'''
 
+	_archive_mutation = '''
+	mutation {
+		archiveProject(
+			lucidId: {p.id}
+		){
+			id
+			openForTimeLogging
+		}
+	}
+	'''
+
+	_unarchive_mutation = '''
+	mutation {
+		unarchiveProject(
+			lucidId: {p.id}
+		){
+			id
+			openForTimeLogging
+		}
+	}
+	'''
     def __init__(self):
         '''
         create graphql client
@@ -86,73 +100,54 @@ class Service(service_template.ServiceTemplate):
 		self.create(service_connection_id)
 
 	def archive(self, service_connection_id):
-		pass
+		ServiceConnection = apps.get_model("lucid_api", "ServiceConnection")
+        connection = ServiceConnection.objects.get(pk=service_connection_id)
+        project = connection.project
+        try:
+            result = self._client.execute(
+                self._archive_mutation.format(p=project))
+
+            connection.identifier = result.id
+			if not result.openForTimeLogging
+				connection.state_message = "Success: No longer able to log time on Lucille project id: {}".format(
+					result.id)
+				connection.save()
+			else:
+				connection.state_message = "FAILED: {}".format(
+					result)
+				connection.save()
+				raise LucilleException('Project did not disable time logging')
+            self._logger.info(
+                'Successfully created project %s-%s in lucille (id:%s)', project.id, project.title, result.id)
+        except e as Exception:
+            self._logger.error(
+                'Error creating project in Lucille: %s', e, exc_info=True)
+            raise e
 
 	def unarchive(self, service_connection_id):
-		pass
-		
-
-    def _setup_logger(self, level=settings.LOG_LEVEL_TYPE, to_file=True):
-        '''
-        DEPRECIATED AFTER MOVE TO DJANGO/CELERY
-        Sets up the logger for the service
-
-        Args:
-                level (str): logging level as a string "info", "debug", "warn", "error", "critical"
-        '''
-
-        logger = logging.getLogger(type(self).__name__)
-        assert isinstance(logger, logging.Logger)
-
+		ServiceConnection = apps.get_model("lucid_api", "ServiceConnection")
+        connection = ServiceConnection.objects.get(pk=service_connection_id)
+        project = connection.project
         try:
-            logging_path = os.environ['LOG_PATH']
-        except KeyError:
-            logging_path = "/logs"
+            result = self._client.execute(
+                self._unarchive_mutation.format(p=project))
 
-        if to_file and not settings.IS_HEROKU:
-            if not os.path.isdir(logging_path):
-                os.mkdir(logging_path)
-            log_path = os.path.join(
-                logging_path, '{}.log'.format(type(self).__name__))
-            handler = logging.FileHandler(log_path)
-            formatter = logging.Formatter(
-                '%(asctime)s | %(levelname)-7s| %(module)s.%(funcName)s :: %(message)s')
-            handler.setFormatter(formatter)
-            logger.addHandler(handler)
+			if result.openForTimeLogging
+				connection.state_message = "Success: Now able to log time on Lucille project id: {}".format(
+					result.id)
+				connection.save()
+			else:
+				connection.state_message = "FAILED: {}".format(
+					result)
+				connection.save()
+				raise LucilleException('Project did not enable time logging')
+            self._logger.info(
+                'Successfully created project %s-%s in lucille (id:%s)', project.id, project.title, result.id)
+        except e as Exception:
+            self._logger.error(
+                'Error creating project in Lucille: %s', e, exc_info=True)
+            raise e
+		 
 
-        elif settings.IS_HEROKU:
-            handler = logging.StreamHandler(sys.stdout)
-            formatter = logging.Formatter(
-                '%(levelname)-7s| %(module)s.%(funcName)s :: %(message)s')
-            handler.setFormatter(formatter)
-            logger.addHandler(handler)
-
-        return logger
-
-    def get_pretty_name(self):
-        return self._pretty_name
-
-    def get_link(self, project_id):
-        return ""
-
-    def get_link_dict(self, project_id):
-        return {self.get_pretty_name(): self.get_link(project_id)}
-
-    def _format_slug(self, connection):
-        '''
-        Formats the slug based on the connection data.
-
-        # Args:
-        - **connection**: a *connection* model object from lucid-api
-        '''
-
-        return self._DEFAULT_FORMAT.format(
-            typecode=connection.project.type_code.character_code,
-            project_id=connection.project.id,
-            title=connection.project.title,
-            connection_name=connection.connection_name
-        )
-
-
-class ServiceException(Exception):
-    pass
+class LucilleException( service_template.ServiceException):
+	pass
